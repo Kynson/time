a8b9969b1bb00a54ce37e1c43a3314bc393a95a0
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2024-06-06 12:01:18 +0800

    Initial Commit

---
+++ rainmeter/rainmeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  rainmeterApp.swift
+//  rainmeter
+//
+//  Created by Kynson Szetau on 6/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct rainmeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
